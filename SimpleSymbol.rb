f7a3bd9e8514afff5b44a728b2f71726467ccf63
LeeJaeChoul <cnrzo114@naver.com>
2015-07-31 16:32:58 +0900

    Create SimpleSymbol.rb

---
+++ SimpleSymbol.rb
@@ -0,0 +1,12 @@
+def SimpleSymbols(str)
+patient1 = { :ruby => "red" }
+patient2 = { :ruby => "programming" }
+
+patient1.each_key {|key| puts key.object_id.to_s}
+3918094
+patient2.each_key {|key| puts key.object_id.to_s}
+3918094
+  # code goes here
+  return str 
+         
+end
